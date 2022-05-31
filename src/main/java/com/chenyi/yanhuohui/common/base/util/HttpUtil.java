d7d3c08debdb041e3ac2cbbf0d1cceed13afe0b1
chenchenchenyi <614362234@qq.com>
2022-05-31 11:24:44 +0800

    FEAT:增加获取response和request的工具类

---
+++ src/main/java/com/chenyi/yanhuohui/common/base/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.chenyi.yanhuohui.common.base.util;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public final class HttpUtil {
+
+    public static HttpServletRequest getRequest(){
+        return ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+    }
+
+    public static HttpServletResponse getReponse(){
+        return ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getResponse();
+    }
+}
